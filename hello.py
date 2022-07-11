fd4f668e0726e08fd2f0024190a40b23151f2289
Aluandalee <contato.aluandalee@gmail.com>
2022-07-11 14:44:19 -0300

    Adicionado SHEBANG e um comentário

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-print ("Hello, world!")
+#!python3.10.5 | \Users\X\AppData\Local\Programs\
+# Este programa imprime Hello World
+
+print ("Hello, world!") # test-ignore
 print('Bernardo'.upper())
 print (35 + 7)
